4ef9650c5eb4c9fcde851c6aa07ce740d18bbf28
Антонина <antonina@Ruslan-MacBook-Pro.local>
2022-11-28 12:20:31 +0700

    Место ПЗ4

---
+++ scripts/index.js
@@ -0,0 +1,52 @@
+let profileName = "Жак-Ив Кусто";
+let profileDescription = "Исследователь океана";
+
+function openPopup() {
+    const popup = document.querySelector(".popup");
+    popup.classList.add("open"); 
+
+    document.querySelector(".form__text-name").textContent = profileName;
+    document.querySelector(".form__text-description").textContent = profileDescription; 
+}
+
+function closePopup() {
+    const popup = document.querySelector(".popup");
+    popup.classList.remove("open"); 
+}
+
+function refreshData() {
+    document.querySelector(".profile__title").textContent = profileName;
+    document.querySelector(".profile__description").textContent = profileDescription;
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    profileName = document.querySelector(".form__text-name").value;
+    profileDescription = document.querySelector(".form__text-description").value;
+
+    refreshData();
+    closePopup();
+}; 
+
+window.addEventListener("load", () => {
+    const buttonEdit = document.querySelector(".profile__edit-button");
+    buttonEdit.addEventListener("click", openPopup);
+ 
+    const modalCloseButton = document.querySelector(".popup__button-close");  
+    modalCloseButton.addEventListener("click", closePopup);
+
+    const formElement = document.querySelector(".form");
+    formElement.addEventListener("submit", onSubmit);
+
+    refreshData();
+});
+
+function toggleLike() {
+    this.classList.toggle("element__like-button_active");
+}
+
+window.addEventListener("load", () => {
+    const likes = document.querySelectorAll(".elements__item-like-button");
+    likes.forEach(x => x.addEventListener("click", toggleLike));
+});
